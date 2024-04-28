afafb9b84080e5b1497c338ce1cd347fb16cfa8e
taianekas <taianekas.dev@gmail.com>
2024-04-28 15:08:56 -0300

    Componente: Avatar

--- ignite/frontend/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ ignite/frontend/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,13 +1,18 @@
 import styles from './Sidebar.module.css'
 import ImageCapa from '../assets/imagem-capa.jpg'
 import { PencilLine } from "@phosphor-icons/react";
+import { Avatar } from './Avatar';
 
 export function Sidebar () {
   return (
     <aside className = {styles.sidebar} >
       <img className = {styles.cover} src= {ImageCapa} alt= 'Imagem de capa'/>
       <div className = {styles.profile} >
-        <img className = {styles.avatar} src = 'https://github.com/duronne.png' alt= 'Imagem de usuario' />
+
+        <Avatar 
+          src = {'https://github.com/duronne.png'}
+          alt = {'Imagem de usuario'}
+        />
         <strong>Taiane Karine</strong>
         <span>Web Developer</span>
       </div>
