03ad72056e82f52042408b73fd0213f57c9704dc
taianekas <taianekas.dev@gmail.com>
2024-04-28 15:43:18 -0300

    Iterando no JSX

--- ignite/frontend/aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ ignite/frontend/aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -2,7 +2,9 @@ import styles from './Post.module.css'
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
 
-export function Post () {
+export function Post (props) {
+
+  console.log(props)
   return (
     <article className = {styles.post} >
       <header>
