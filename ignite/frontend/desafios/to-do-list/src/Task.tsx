ad21c3066793d49c172ee9d2c66e143b51c6b51e
taianekas <taianekas.dev@gmail.com>
2024-04-29 15:27:57 -0300

    Implementado interacao na aplicacao

--- ignite/frontend/desafios/to-do-list/src/Task.tsx
+++ ignite/frontend/desafios/to-do-list/src/Task.tsx
@@ -1,25 +1,53 @@
+import {v4 as uuidv4} from 'uuid'
 import styles from './Task.module.css'
 import { Empty } from './Empty'
 import { TaskList } from './TaskList'
+import { TaskListProps } from './TaskList'
 
-export function Task () {
+export interface TaskProps {
+  countCreatedTasks: number,
+  countCompletedTasks: number,
+}
+
+export function Task ({ countCreatedTasks, countCompletedTasks }: TaskProps) {
+  const listTasks: TaskListProps[] = [
+    {
+      id: uuidv4(),
+      isChecked: true,
+      text: 'Primeira tarefa',
+    },
+    {
+      id: uuidv4(),
+      isChecked: false,
+      text: 'Segunda tarefa',
+    },
+  ]
+
   return (
     <div className = {styles.content}>
       <div className = {styles.taskStatus}>
         <p className = {styles.createdTasks}>
           Tarefas criadas
-          <span>0</span>
+          <span>{countCreatedTasks}</span>
         </p>
         <p className = {styles.completedTasks}>
           Concluídas
-          <span>0</span>
+          <span>{countCompletedTasks}</span>
         </p>
       </div>
       <div className = {styles.contentTasks}>
-        <Empty />
-        <TaskList />
-        <TaskList />
-        <TaskList />
+        {
+          listTasks.length > 0 ? (
+
+          listTasks.map((item) => (
+            <TaskList
+              key = {item.id}
+              text = {item.text}
+              isChecked = {item.isChecked}
+            />
+          ))
+          ) : <Empty />
+        }
       </div>
     </div>
   )
--- ignite/frontend/desafios/to-do-list/src/TaskList.tsx
+++ ignite/frontend/desafios/to-do-list/src/TaskList.tsx
@@ -1,28 +1,32 @@
 import styles from './TaskList.module.css'
 import { Trash, Check } from '@phosphor-icons/react'
 
+export interface TaskListProps {
+  id?: string
+  text: string
+  isChecked: boolean
+}
 
-export function TaskList () {
-  const isChecked = false
+export function TaskList ({ text, isChecked }: TaskListProps) {
+
   const checkboxCheckedClassname = isChecked ? styles['checkbox-checked'] : styles['checkbox-unchecked']
   const paragraphCheckedClassname = isChecked ? styles['paragraph-checked'] : ''
 
-
   return (
     <div className = {styles.containerWithTasks}>
-      <label htmlFor="checkbox" >
-        <input readOnly type="checkbox" checked={isChecked} />
+      <label htmlFor='checkbox' >
+        <input readOnly type='checkbox' checked={isChecked} />
         <span className={`${styles.checkBox} ${checkboxCheckedClassname}`}>
           {isChecked && <Check size={17} />}
         </span>
 
         <p className={`${styles.paragraph} ${paragraphCheckedClassname}`}>
-          Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+          { text }
         </p>
       </label>
 
       <button>
-        <Trash size={17} color="#808080" />
+        <Trash size={17} color='#808080' />
       </button>
     </div>
   )
