b5285b78dcfee0d001c5614f8d300ad2f17c4b36
taianekas <taianekas.dev@gmail.com>
2024-04-29 02:38:54 -0300

    header criado

--- ignite/frontend/desafios/to-do-list/src/App.tsx
+++ ignite/frontend/desafios/to-do-list/src/App.tsx
@@ -1,7 +1,15 @@
 import './global.css'
+import styles from './App.module.css'
+import Logo from './assets/logo.svg'
 
 export function App() {
-  return
+  return (
+    <div>
+      <header className = {styles.header}>
+        <img src = { Logo } alt = 'Logotipo do site ToDo List' />
+      </header>
+    </div>
+  )
 }
 
 
