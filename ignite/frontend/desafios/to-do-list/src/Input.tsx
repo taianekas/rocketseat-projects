2c6aaf4af6c06c13773983270a229f12538cbafd
taianekas <taianekas.dev@gmail.com>
2024-04-29 03:19:06 -0300

    Correcao className input

--- ignite/frontend/desafios/to-do-list/src/Input.tsx
+++ ignite/frontend/desafios/to-do-list/src/Input.tsx
@@ -4,7 +4,7 @@ import styles from './Input.module.css'
 export function Input () {
   return (
     <div className = {styles.wrapper}>
-      <input className = {styles.search} type = {'text'} placeholder = {'Adicione uma nova tarefa'}/>
+      <input className = {styles.newTask} type = {'text'} placeholder = {'Adicione uma nova tarefa'}/>
 
       <button className = {styles.btnCreate} type = {'submit'}>
         Criar
