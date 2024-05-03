5f9b8067e481f18fc0a75df9c30d90258915c98a
taianekas <taianekas.dev@gmail.com>
2024-05-03 20:54:46 -0300

    Add estilos globais e  componente header

---
+++ ignite/frontend/desafios/coffee-delivery/src/pages/Router.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './Home'
+import { Checkout } from './Checkout'
+import { CheckoutFilled } from './Checkout-Filled'
+import { Sucess } from './Sucess'
+import { DefaultLayout } from '../layouts/defaultLayout'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/checkout-filled" element={<CheckoutFilled />} />
+      <Route path="/success" element={<Sucess />} />
+    </Routes>
+  )
+}
