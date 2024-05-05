fd2d39b92a42325c2e89c14514010c395085296e
taianekas <taianekas.dev@gmail.com>
2024-05-05 10:53:10 -0300

    Adicionada funcionalidade nos botoes de Counter

--- ignite/frontend/desafios/coffee-delivery/src/components/Counter/index.tsx
+++ ignite/frontend/desafios/coffee-delivery/src/components/Counter/index.tsx
@@ -1,16 +1,41 @@
 import { Minus, Plus } from '@phosphor-icons/react'
 import { Button, Container } from './styles'
+import { useState } from 'react'
 
 export function Counter() {
+  const [value, setValue] = useState(0)
+
+  function handleUpdatedCountItem(action: string) {
+    if (action === 'add') {
+      const result = value + 1
+      setValue(result)
+    }
+
+    if (action === 'remove') {
+      if (!(value <= 0)) {
+        const result = value - 1
+        return setValue(result)
+      }
+    }
+  }
+
   return (
     <Container>
-      <Button>
+      <Button
+        onClick={() => {
+          handleUpdatedCountItem('remove')
+        }}
+      >
         <Minus weight="bold" />
       </Button>
 
-      <p>{'1'}</p>
+      <input type="number" value={value} />
 
-      <Button>
+      <Button
+        onClick={() => {
+          handleUpdatedCountItem('add')
+        }}
+      >
         <Plus weight="bold" />
       </Button>
     </Container>
