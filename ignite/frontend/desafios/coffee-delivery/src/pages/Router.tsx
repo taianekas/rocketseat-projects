6baee4b6c591626ea532f0750bf5110e705aad84
taianekas <taianekas.dev@gmail.com>
2024-05-05 22:20:52 -0300

    Adiciona funcionalidade de alterar preco conforme o contador altera

--- ignite/frontend/desafios/coffee-delivery/src/pages/Router.tsx
+++ ignite/frontend/desafios/coffee-delivery/src/pages/Router.tsx
@@ -3,6 +3,7 @@ import { Home } from './Home'
 import { Checkout } from './Checkout'
 import { Sucess } from './Sucess'
 import { DefaultLayout } from '../layouts/defaultLayout'
+import { Testes } from './testes'
 
 export function Router() {
   return (
@@ -11,6 +12,7 @@ export function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
 
+        <Route path="/testes" element={<Testes />} />
         <Route path="/success" element={<Sucess />} />
       </Route>
     </Routes>
