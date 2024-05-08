64aed0d25595d89775eeab88562ca41dfb36780f
taianekas <taianekas.dev@gmail.com>
2024-05-08 02:14:55 -0300

    Responsabilidade de alterar estado de count para o useContext

--- ignite/frontend/desafios/coffee-delivery/src/components/Actions/index.tsx
+++ ignite/frontend/desafios/coffee-delivery/src/components/Actions/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import {
   Container,
   AddToCart,
@@ -9,7 +9,10 @@ import {
 } from './styled'
 import { Counter } from '../Counter'
 import { ShoppingCartSimple, Trash } from '@phosphor-icons/react'
-import { ProductData } from '../../contexts/ShoppingCartContext'
+import {
+  ProductData,
+  ShoppingCartContext,
+} from '../../contexts/ShoppingCartContext'
 
 interface ActionsProps {
   action: 'add' | 'delete'
@@ -17,16 +20,11 @@ interface ActionsProps {
 }
 
 export function Actions({ action, data }: ActionsProps) {
-  const [amountCounter, setAmountCounter] = useState(0)
+  const { productCounts } = useContext(ShoppingCartContext)
   const [productNewData, setProductNewData] = useState([{ ...data }])
-
-  function updatedCount(count: number) {
-    setAmountCounter(count)
-  }
 
   function coinFormat(value: number) {
     const twoDecimalsPlaces = Math.round(value * 100) / 100
-
     const valueInString = twoDecimalsPlaces.toFixed(2)
 
     const [integerPart, decimalPart] = valueInString.split('.')
@@ -39,20 +37,6 @@ export function Actions({ action, data }: ActionsProps) {
     return `${integerPartFormatada},${decimalPart}`
   }
 
-  function handleAddNewProduct(data: ProductData) {
-    const newProduct = productNewData.map((item) => {
-      if (item.id === data.id) {
-        return {
-          ...item,
-          count: amountCounter,
-          price: amountCounter !== 0 ? data.price * amountCounter : item.price,
-        }
-      }
-      return item
-    })
-    setProductNewData(newProduct)
-  }
-
   function handleRemoveProduct(id: string) {
     const newProduct = productNewData.filter((item) => {
       return item.id !== id
@@ -60,7 +44,10 @@ export function Actions({ action, data }: ActionsProps) {
     setProductNewData(newProduct)
   }
 
-  const newPrice = amountCounter > 0 ? amountCounter * data.price : data.price
+  const price =
+    productCounts[data.id] > 0
+      ? data.price * productCounts[data.id]
+      : data.price
 
   const willBeDisplayed = action
   return (
@@ -69,16 +56,12 @@ export function Actions({ action, data }: ActionsProps) {
         {willBeDisplayed === 'add' ? (
           <AddToCart>
             <p>
-              R$ <strong>{coinFormat(newPrice)}</strong>
+              R$ <strong>{coinFormat(price)}</strong>
             </p>
 
-            <Counter onAddCount={updatedCount} onDeleteCount={updatedCount} />
+            <Counter data={data} />
 
-            <ButtonAddItem
-              onClick={() => {
-                handleAddNewProduct({ ...data })
-              }}
-            >
+            <ButtonAddItem onClick={() => {}}>
               <ShoppingCartSimple weight={'fill'} size={22} />
             </ButtonAddItem>
           </AddToCart>
@@ -88,7 +71,7 @@ export function Actions({ action, data }: ActionsProps) {
 
             <Details className="dois">
               <p>Expresso</p>
-              <Counter onAddCount={updatedCount} onDeleteCount={updatedCount} />
+              <Counter data={data} />
               <ButtonRemoveItem onClick={() => handleRemoveProduct(data.id)}>
                 <Trash size={16} />
                 Remover
@@ -96,7 +79,7 @@ export function Actions({ action, data }: ActionsProps) {
             </Details>
 
             <p>
-              <strong>{`R$ ${coinFormat(newPrice)}`}</strong>
+              <strong>{`R$ ${coinFormat(price)}`}</strong>
             </p>
           </RemoveFromCart>
         )}
