20610592d33f5e9cf9ebedc786cb9140d43fc8a3
taianekas <taianekas.dev@gmail.com>
2024-05-08 04:46:33 -0300

    Adiciona itens no carrinho de compras

--- ignite/frontend/desafios/coffee-delivery/src/components/Cart/index.tsx
+++ ignite/frontend/desafios/coffee-delivery/src/components/Cart/index.tsx
@@ -1,15 +1,19 @@
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../contexts/ShoppingCartContext'
 import { ShoppingCart } from '@phosphor-icons/react'
 import { Container, Count } from './styles'
 
 export function Cart() {
-  const countCart = 0
+  const { countCart } = useContext(ShoppingCartContext)
+
+  const count: number = countCart
   const purchasesInCart = countCart > 0 ? 'visible' : 'hidden'
 
   return (
     <Container>
       <ShoppingCart size={22} color="#C47F17" weight="fill" />
       <Count>
-        <p className={`count-${purchasesInCart}`}>{countCart}</p>
+        <p className={`count-${purchasesInCart}`}>{count}</p>
       </Count>
     </Container>
   )
--- ignite/frontend/desafios/coffee-delivery/src/contexts/ShoppingCartContext.tsx
+++ ignite/frontend/desafios/coffee-delivery/src/contexts/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, createContext, useState } from 'react'
+import { ReactNode, createContext, useEffect, useState } from 'react'
+import { ProductsList } from '../data'
 
 // ➜ Sempre que precisar representar um único produto em seu aplicativo.
 // Por exemplo, ao trabalhar com dados de um produto em um componente de exibição
@@ -17,16 +18,22 @@ export interface ProductData {
 // Por exemplo, ao trabalhar com uma lista de produtos no contexto de um carrinho de
 // compras ou ao passar dados de produtos para um componente que espera uma lista de
 // produtos.
-export interface TypesDataProduct {
-  dataBase: ProductData[]
-}
+export interface TypesDataProduct {}
 
 type ProductId = string
 type ProductCount = number
 type ProductCounts = Record<ProductId, ProductCount>
 
+interface CartTypes {
+  id: ProductId
+  count: ProductCount
+}
+
 interface ShoppingCartContextTypes {
+  countCart: number
+  cartProducts: CartTypes[]
   productCounts: ProductCounts
+  productsDataBase: ProductData[]
   decrementCount: (value: ProductId) => void
   incrementCount: (value: ProductId) => void
 }
@@ -40,8 +47,21 @@ export function ShoppingCartContextProvider({
   children,
 }: ShoppingCartContextProviderProps) {
   const [productCounts, setProductCounts] = useState<ProductCounts>({})
+  const [countCart, setCountCart] = useState<number>(0)
+
+  const productsDataBase: ProductData[] = ProductsList.map((data) => ({
+    id: data.id,
+    tag: data.tag.map((item) => item.title),
+    name: data.name,
+    description: data.description,
+    price: data.price,
+    image: data.image,
+    count: data.count,
+  }))
 
   function decrementCount(id: ProductId) {
+    console.log(typeof productCounts)
+
     setProductCounts((prevProductsCount) => ({
       ...prevProductsCount,
       [id]: (prevProductsCount[id] ?? 0) !== 0 ? prevProductsCount[id] - 1 : 1,
@@ -55,12 +75,31 @@ export function ShoppingCartContextProvider({
     }))
   }
 
+  useEffect(() => {
+    const updateList: CartTypes[] = Object.entries(productCounts).map(
+      ([id, count]) => ({
+        id,
+        count,
+      }),
+    )
+
+    const totalCount = updateList.reduce((acc, curr) => acc + curr.count, 0)
+
+    setCountCart(totalCount)
+  }, [productCounts])
+
   return (
     <ShoppingCartContext.Provider
       value={{
+        countCart,
         productCounts,
         decrementCount,
         incrementCount,
+        productsDataBase,
+        cartProducts: Object.entries(productCounts).map(([id, count]) => ({
+          id,
+          count,
+        })),
       }}
     >
       {children}
