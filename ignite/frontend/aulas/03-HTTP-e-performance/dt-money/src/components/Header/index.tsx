28c62cb3e471419e26c4180f506a7b87b9059823
taianekas <taianekas.dev@gmail.com>
2024-05-15 13:14:25 -0300

    Estrutura visual - Criando um modal acessível

--- ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/Header/index.tsx
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
  import logoImg from "../../assets/logo.svg";
 
@@ -7,8 +8,21 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
      <HeaderContainer>
        <HeaderContent>
          <img src={logoImg} alt="" />
+         <Dialog.Root>
+         <Dialog.Trigger asChild>
+           <NewTransactionButton>Nova transação</NewTransactionButton>
+         </Dialog.Trigger>
 
-         <NewTransactionButton>Nova transação</NewTransactionButton>
+         <Dialog.Portal>
+           <Dialog.Overlay />
+
+           <Dialog.Content>
+             <Dialog.Title>Nova Transação</Dialog.Title>
+
+             <Dialog.Close />
+           </Dialog.Content>
+         </Dialog.Portal>
+       </Dialog.Root>
        </HeaderContent>
      </HeaderContainer>
    );
