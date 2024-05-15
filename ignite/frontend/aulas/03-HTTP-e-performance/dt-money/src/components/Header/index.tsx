3f5c798537f5d643e7c43121456c330b18267966
taianekas <taianekas.dev@gmail.com>
2024-05-15 13:00:39 -0300

    Estrutura visual - Componente: Header

---
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+ import logoImg from "../../assets/logo.svg";
+
+ export function Header() {
+   return (
+     <HeaderContainer>
+       <HeaderContent>
+         <img src={logoImg} alt="" />
+
+         <NewTransactionButton>Nova transação</NewTransactionButton>
+       </HeaderContent>
+     </HeaderContainer>
+   );
+ }
+ 
