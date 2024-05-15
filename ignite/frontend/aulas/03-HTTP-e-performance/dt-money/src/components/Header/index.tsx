2f9e894b4b456732e49549576f6f0b8f27ce5331
taianekas <taianekas.dev@gmail.com>
2024-05-15 13:33:52 -0300

    Estrutura visual - Modal de nova transação

--- ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/Header/index.tsx
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/Header/index.tsx
@@ -14,16 +14,6 @@ export function Header() {
         </Dialog.Trigger>
 
         <NewTransactionModal />
-
-        <Dialog.Portal>
-          <Dialog.Overlay />
-
-          <Dialog.Content>
-            <Dialog.Title>Nova Transação</Dialog.Title>
-
-            <Dialog.Close />
-          </Dialog.Content>
-        </Dialog.Portal>
       </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
