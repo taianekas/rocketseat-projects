27b5fe42591bd5d9259d8b1d60675349b4e8dc7a
taianekas <taianekas.dev@gmail.com>
2024-05-15 13:47:05 -0300

    Estrutura visual - Botões de entrada e saída

--- ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/NewTransactionModal/index.tsx
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from "phosphor-react";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
-import { CloseButton, Content, Overlay } from './styles';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
 export function NewTransactionModal() {
   return (
@@ -20,6 +20,16 @@ export function NewTransactionModal() {
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
+          <TransactionType>
+             <TransactionTypeButton variant="income">
+               <ArrowCircleUp size={24} />
+               Entrada
+             </TransactionTypeButton>
+             <TransactionTypeButton variant="outcome">
+               <ArrowCircleDown size={24} />
+               Saída
+             </TransactionTypeButton>
+           </TransactionType>
           <button type="submit">
             Cadastrar
           </button>
