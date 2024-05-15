0af8cbe5e5b53d569e835c2f3ec3ce3b401ef545
taianekas <taianekas.dev@gmail.com>
2024-05-15 15:00:11 -0300

    Conexão com a API - Criando contexto de transações

--- ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/App.tsx
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/App.tsx
@@ -1,16 +1,18 @@
 import { ThemeProvider } from 'styled-components';
- import { GlobalStyle } from './styles/global';
- import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 import { Transactions } from './pages/Transactions';
+import { TransactionsProvider } from './contexts/TransactionsContext';
 
 export function App() {
-  
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
