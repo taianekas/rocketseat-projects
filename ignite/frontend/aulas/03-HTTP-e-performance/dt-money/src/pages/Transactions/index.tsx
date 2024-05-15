9a9c7cdd539f8d8ca8ba2bb48257fbeab5761ea1
taianekas <taianekas.dev@gmail.com>
2024-05-15 14:44:51 -0300

    Conexão com a API - Realizando requisição HTTP

--- ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/pages/Transactions/index.tsx
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/pages/Transactions/index.tsx
@@ -1,42 +1,62 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
- export function Transactions() {
-   return (
-     <div>
-      <Header />
-      <Summary/>
+interface Transactions {
+  id: number,
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
 
-       <TransactionsContainer>
-        <SearchForm />
 
-         <TransactionsTable>
-           <tbody>
-             <tr>
-               <td width="50%">Desenvolvimento de site</td>
-               <td>
-                 <PriceHighlight variant="income">
-                   R$ 12.000,00
-                 </PriceHighlight>
-               </td>
-               <td>Venda</td>
-               <td>13/04/2022</td>
-             </tr>
-             <tr>
-               <td width="50%">Hambúrguer</td>
-               <td>
-                 <PriceHighlight variant="outcome">
-                   -R$ 59,00
-                 </PriceHighlight>
-               </td>
-               <td>Alimentação</td>
-               <td>10/04/2022</td>
-             </tr>
-           </tbody>
-         </TransactionsTable>
-       </TransactionsContainer>
-     </div>
-   );
- }
+export function Transactions() {
+  const [transactions, setTransactions] = useState<Transactions[]>([])
+
+  const loadTransactions = async () => {
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
+  return (
+    <div>
+    <Header />
+    <Summary/>
+
+      <TransactionsContainer>
+      <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            {
+              transactions.map((transaction) => {
+                return (
+                  <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      R$ {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+                )
+              })
+            }
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+}
