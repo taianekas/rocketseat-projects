1c642064b6a63dcce67650cf6b1dff71522b032d
taianekas <taianekas.dev@gmail.com>
2024-05-15 13:00:39 -0300

    Estrutura visual - Componente: Header

--- ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/App.tsx
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components';
  import { GlobalStyle } from './styles/global';
  import { defaultTheme } from './styles/themes/default';
+import { Transactions } from './pages/Transactions';
 
 export function App() {
   
@@ -9,7 +10,7 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <h1>Hello World</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
