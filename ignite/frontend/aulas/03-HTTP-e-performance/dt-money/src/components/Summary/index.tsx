baa35b11c295d08b8178c861ca595af594b5bd99
taianekas <taianekas.dev@gmail.com>
2024-05-15 15:09:44 -0300

    Conexão com a API - Calculando resumo de transações

--- ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/Summary/index.tsx
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/Summary/index.tsx
@@ -6,6 +6,17 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
  export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+
+    return acc
+  }, {income: 0, outcome: 0, total: 0})
    return (
      <SummaryContainer>
        <SummaryCard>
@@ -14,7 +25,7 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
            <ArrowCircleUp size={32} color='#00b37e' />
          </header>
 
-         <strong>R$ 17.400,00</strong>
+         <strong>{summary.income}</strong>
        </SummaryCard>
 
        <SummaryCard>
@@ -23,7 +34,7 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
            <ArrowCircleDown size={32} color='#f75a68' />
          </header>
 
-         <strong>R$ 17.400,00</strong>
+         <strong>{summary.outcome}</strong>
        </SummaryCard>
 
        <SummaryCard variant='green'>
@@ -32,7 +43,7 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
            <CurrencyDollar size={32} color='#fff' />
          </header>
 
-         <strong>R$ 17.400,00</strong>
+         <strong>{summary.total}</strong>
        </SummaryCard>
      </SummaryContainer>
    );
