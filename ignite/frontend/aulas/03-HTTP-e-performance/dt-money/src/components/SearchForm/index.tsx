c8b900325cee7c8beaa7b79c5081ae9e035cd8e3
taianekas <taianekas.dev@gmail.com>
2024-05-15 17:20:39 -0300

    Conexão com a API - Buscando transações

--- ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/SearchForm/index.tsx
+++ ignite/frontend/aulas/03-HTTP-e-performance/dt-money/src/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as zod from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 const searchFormSchema = zod.object({
   query: zod.string()
@@ -11,22 +13,27 @@ const searchFormSchema = zod.object({
 type SearchFormInputs = zod.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-const { register, handleSubmit, formState: {isSubmitting} } = useForm<SearchFormInputs>({
-  resolver: zodResolver(searchFormSchema)
-})
+const { fetchTransactions } = useContext(TransactionsContext)
 
-function handleSearchTransactions(data: SearchFormInputs) {
-  console.log(data)
+const {
+  register,
+  handleSubmit,
+ formState: { isSubmitting },
+} = useForm<SearchFormInputs>({
+ resolver: zodResolver(searchFormSchema),
+});
+
+async function handleSearchTransactions(data: SearchFormInputs) {
+  await fetchTransactions(data.query)
 }
 
   return (
     <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
-      <input type="text" placeholder="Busque por transações" {...register('query')}/>
-
-      <button type="submit" disabled={isSubmitting}>
-        <MagnifyingGlass size={20} />
-        Buscar
-      </button>
-    </SearchFormContainer>
-  );
+    <input type="text" placeholder="Busque por transações" {...register('query')} />
+    <button type="submit" disabled={isSubmitting}>
+      <MagnifyingGlass size={20} />
+      Buscar
+    </button>
+  </SearchFormContainer>
+);
 }
