b7f5a01ea1c001baea494ec27a99aa773f76edb1
taianekas <taianekas.dev@gmail.com>
2024-05-16 14:34:35 -0300

    🎨 Estrutura visual - Componente: PostCardInfo

---
+++ ignite/frontend/desafios/github-blog/src/pages/Post/index.tsx
@@ -0,0 +1,10 @@
+import { PostCardInfo } from './Components/PostCardInfo'
+import { Container } from './styles'
+
+export function Post() {
+  return (
+    <Container>
+      <PostCardInfo />
+    </Container>
+  )
+}
---
+++ ignite/frontend/desafios/github-blog/src/pages/Post/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  width: 86.4rem;
+  background-color: transparent;
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
