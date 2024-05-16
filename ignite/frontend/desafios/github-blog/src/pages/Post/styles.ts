28942a0639892d6749e6ce742c598e433701361b
taianekas <taianekas.dev@gmail.com>
2024-05-16 18:38:33 -0300

    ✨ Conexão com a API - Page: Post

--- ignite/frontend/desafios/github-blog/src/pages/Post/styles.ts
+++ ignite/frontend/desafios/github-blog/src/pages/Post/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.main`
   width: 86.4rem;
-  position: absolute;
+  position: fixed;
   top: 30%;
   left: 50%;
   transform: translate(-50%, -50%);
