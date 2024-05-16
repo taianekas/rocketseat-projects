1a7269d057da5c4cae8e4838cc3038ab3df4a891
taianekas <taianekas.dev@gmail.com>
2024-05-16 20:37:42 -0300

    ⚙ Instalando React Router DOM

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/App.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/App.tsx
@@ -1,10 +1,7 @@
 import './global.css'
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 export function App() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>Enviar</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
