1f43d9bc32336bc5e6c050f1a946dd495333d658
taianekas <taianekas.dev@gmail.com>
2024-05-16 18:22:49 -0300

    ✨ Conexão com a API - PostCard

--- ignite/frontend/desafios/github-blog/src/pages/Home/Components/PostCard/index.tsx
+++ ignite/frontend/desafios/github-blog/src/pages/Home/Components/PostCard/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import { PostProps } from '../..'
 import { formatDistanceDate } from '../../../../utils/formatter'
 
@@ -7,11 +8,17 @@ interface PostCardProps {
   data: PostProps
 }
 export function PostCard({ data }: PostCardProps) {
+  const navigate = useNavigate()
+
+  function handleCompletePost() {
+    navigate(`/post/${data.number}`)
+  }
+
   return (
-    <Container>
+    <Container onClick={handleCompletePost}>
       <Title>
         <h1>{data.title}</h1>
-        <span>{formatDistanceDate(data.created_at)}</span>
+        <span>{data.created_at && formatDistanceDate(data.created_at)}</span>
       </Title>
 
       <div className="description">{data.body}</div>
