a42ef3b1301b1b1dd3e63a3f31d54f90ccbc8cfd
taianekas <taianekas.dev@gmail.com>
2024-05-16 15:08:43 -0300

    🎨 Ajuste visual - Focus Input

--- ignite/frontend/desafios/github-blog/src/styles/global.ts
+++ ignite/frontend/desafios/github-blog/src/styles/global.ts
@@ -14,7 +14,7 @@ export const GlobalStyle = createGlobalStyle`
   
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['base-border']};
+    border: 1px solid ${(props) => props.theme.blue};
   }
 
   ::-webkit-scrollbar-thumb {
