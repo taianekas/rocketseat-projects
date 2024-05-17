183c9bd45ae36650a88cba7251181bc5fe210c3f
taianekas <taianekas.dev@gmail.com>
2024-05-17 12:02:43 -0300

    ✨ Loading state no perfil

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/components/account-menu.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/components/account-menu.tsx
@@ -12,14 +12,16 @@ import {
 import { getManagedRestaurant } from '@/api/get-managed-restaurant'
 import { useQuery } from '@tanstack/react-query'
 import { getProfile } from '@/api/get-profile'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
   })
 
-  const { data: managedRestaurant } = useQuery({
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+  useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
   })
@@ -31,17 +33,30 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          {managedRestaurant?.name}
+          {isLoadingManagedRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+            managedRestaurant?.name
+          )}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            {profile?.email}
-          </span>
+        {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
