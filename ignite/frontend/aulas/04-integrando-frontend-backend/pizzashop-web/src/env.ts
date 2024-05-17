46cf018f82024e67a3301ca13434a5870983eb38
taianekas <taianekas.dev@gmail.com>
2024-05-17 18:23:30 -0300

    Setup do MSW

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/env.ts
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
+  MODE: z.enum(['production', 'development', 'test']),
 })
 
 export const env = envSchema.parse(import.meta.env)
