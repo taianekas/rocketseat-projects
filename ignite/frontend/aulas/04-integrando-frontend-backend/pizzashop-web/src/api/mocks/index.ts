abc06d253941c962020f247e756e468f56a2e7c4
taianekas <taianekas.dev@gmail.com>
2024-05-17 18:46:49 -0300

    Mocks de cadastro e dashboard

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/api/mocks/index.ts
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/api/mocks/index.ts
@@ -3,8 +3,24 @@ import { setupWorker } from 'msw/browser'
 import { env } from '@/env'
 
 import { signInMock } from './sign-in-mock'
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount'
+import { getMonthRevenueMock } from './get-month-revenue'
+import { getPopularProductsMock } from './get-popular-products-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
