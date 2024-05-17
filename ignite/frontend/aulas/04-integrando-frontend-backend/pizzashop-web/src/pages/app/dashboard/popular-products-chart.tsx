88eff0b70af1233e70d919434cb0bb2582b22ffa
taianekas <taianekas.dev@gmail.com>
2024-05-17 15:00:36 -0300

    ✨ Receita no período

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
@@ -6,14 +6,6 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useQuery } from '@tanstack/react-query'
 import { getPopularProducts } from '@/api/get-popular-products'
 
-const data = [
-  { product: 'Pepperoni', amount: 40 },
-  { product: 'Mussarela', amount: 30 },
-  { product: 'Marguerita', amount: 50 },
-  { product: '4 Queijos', amount: 16 },
-  { product: 'Frango frito', amount: 26 },
-]
-
 const COLORS = [
   colors.sky[500],
   colors.amber[500],
