ce86c62707e8b62d91a06c8a046955cd987ff2ee
taianekas <taianekas.dev@gmail.com>
2024-05-16 22:53:48 -0300

    🎨 Layout Component: Header

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/_layouts/auth.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Pizza } from 'lucide-react'
 
 export function AuthLayout () {
   return (
-    <div className="min-h-screen grid grid-cols-2">
+    <div className="min-h-screen grid grid-cols-2 antialiased">
       <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <Pizza className="h-5 w-5" />
