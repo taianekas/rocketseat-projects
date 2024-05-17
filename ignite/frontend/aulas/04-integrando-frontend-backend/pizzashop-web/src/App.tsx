107034d0a83330e645826b758ace4cb9aff540cc
taianekas <taianekas.dev@gmail.com>
2024-05-16 23:13:06 -0300

    🎨 Layout Component: Header finished

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/App.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/App.tsx
@@ -3,12 +3,19 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
-
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+        <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+          <Helmet titleTemplate="%s | pizza.shop" />
+
+          <Toaster richColors />
+
+          <RouterProvider router={router} />
+        </ThemeProvider>
       <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
