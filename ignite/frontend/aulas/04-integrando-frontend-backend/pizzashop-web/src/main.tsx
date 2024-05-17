9cf331b013792a199572c0c3f964612941c6ab9d
taianekas <taianekas.dev@gmail.com>
2024-05-17 18:23:30 -0300

    Setup do MSW

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/main.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { enableMSW } from './api/mocks/index.ts'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+enableMSW().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
