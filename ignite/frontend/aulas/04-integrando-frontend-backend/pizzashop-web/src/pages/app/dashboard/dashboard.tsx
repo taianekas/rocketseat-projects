141ad461b0c3da658bfbb68307e897c22b2e23e6
taianekas <taianekas.dev@gmail.com>
2024-05-17 00:36:23 -0300

    🎨 Componente: Grafico de receita no periodo

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/dashboard/dashboard.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/dashboard/dashboard.tsx
@@ -3,6 +3,7 @@ import { MonthRevenueCard } from './month-revenue-card'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
+import { RevenueChart } from './revenue-chart'
 
 export function Dashboard() {
   return (
@@ -17,6 +18,10 @@ export function Dashboard() {
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
         </div>
+
+        <div className="grid grid-cols-9 gap-4">
+           <RevenueChart />
+         </div>
       </div>
     </>
   )
