67b3147b07f576409ea76c16c2476d174a64bdfa
taianekas <taianekas.dev@gmail.com>
2024-05-17 11:07:44 -0300

    ⚙ Configurando API client

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/App.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/App.tsx
@@ -4,6 +4,8 @@ import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -17,7 +19,10 @@ export function App() {
           <RouterProvider router={router} />
         </ThemeProvider>
       <Toaster richColors />
+      <QueryClientProvider client={queryClient}>
+
       <RouterProvider router={router} />
+      </QueryClientProvider>
     </HelmetProvider>
   )
 }
