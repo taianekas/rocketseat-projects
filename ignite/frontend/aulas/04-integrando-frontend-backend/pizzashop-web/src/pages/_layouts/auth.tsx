c16e066808e5e38ef7e751be068313bc383892f4
taianekas <taianekas.dev@gmail.com>
2024-05-16 21:33:07 -0300

    🎨 Layout Page: Login

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/_layouts/auth.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/_layouts/auth.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Pizza } from 'lucide-react'
 
 export function AuthLayout () {
   return (
-    <div>
-      <h1>Autenticação</h1>
-      <div>
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sn">
+          Painel de parceiro &copy; pizza.shop - ${new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/auth/sign-in.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,10 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
 import { Helmet } from "react-helmet-async";
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="w-[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+            <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro</p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">E-mail</Label>
+              <Input id="email" type="email"/>
+            </div>
+            <Button type="submit" className="w-full">Acessar painel</Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
