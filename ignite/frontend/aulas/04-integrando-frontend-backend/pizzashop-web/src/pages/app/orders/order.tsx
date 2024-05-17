71059ba46da99e850c8318ca7aeb0f9367a050c3
taianekas <taianekas.dev@gmail.com>
2024-05-17 13:18:30 -0300

    ✨ Listagem de pedidos

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/orders/order.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/orders/order.tsx
@@ -10,7 +10,16 @@ import {
 } from '@/components/ui/table'
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { useQuery } from '@tanstack/react-query'
+import { getOrders } from '@/api/get-orders'
+
 export function Orders() {
+
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders,
+  })
+
 return (
   <>
     <Helmet title="Pedidos" />
@@ -35,8 +44,9 @@ return (
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
-                  return <OrderTableRow key={i} />
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
                 })}
               </TableBody>
             </Table>
