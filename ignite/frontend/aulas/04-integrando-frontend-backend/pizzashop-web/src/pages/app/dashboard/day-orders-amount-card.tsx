6d6a86c76ec1ee62c0d23bd34f7cda57c30eb927
taianekas <taianekas.dev@gmail.com>
2024-05-17 15:05:55 -0300

    ✨ Loading das métricas

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/dashboard/day-orders-amount-card.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -3,6 +3,7 @@ import { Utensils } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useQuery } from '@tanstack/react-query'
 import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
+import { MetricCardSkeleton } from './metric-card-skeleton'
 
 export function DayOrdersAmountCard() {
   const { data: dayOrdersAmount } = useQuery({
@@ -17,7 +18,7 @@ export function DayOrdersAmountCard() {
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {dayOrdersAmount && (
+        {dayOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {dayOrdersAmount.amount.toLocaleString('pt-BR')}
@@ -40,6 +41,8 @@ export function DayOrdersAmountCard() {
               )}
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
