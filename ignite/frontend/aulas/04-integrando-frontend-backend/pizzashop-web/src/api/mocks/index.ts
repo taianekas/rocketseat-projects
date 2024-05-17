76d1112faf9d506827d8df3dfe0a57c8f10742f0
taianekas <taianekas.dev@gmail.com>
2024-05-17 18:54:56 -0300

    Mock de perfil e atualização

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/api/mocks/index.ts
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/api/mocks/index.ts
@@ -10,6 +10,9 @@ import { getMonthOrdersAmountMock } from './get-month-orders-amount'
 import { getMonthRevenueMock } from './get-month-revenue'
 import { getPopularProductsMock } from './get-popular-products-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
+import { getProfileMock } from './get-profile-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
+import { updateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
   signInMock,
@@ -20,6 +23,9 @@ export const worker = setupWorker(
   getMonthRevenueMock,
   getDailyRevenueInPeriodMock,
   getPopularProductsMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock,
 )
 
 export async function enableMSW() {
