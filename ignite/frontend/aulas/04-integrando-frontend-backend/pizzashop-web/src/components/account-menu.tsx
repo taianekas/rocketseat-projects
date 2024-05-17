7f67c352c525e57e41d0309fe5331b9289132f57
taianekas <taianekas.dev@gmail.com>
2024-05-17 11:45:01 -0300

    ✨ Perfil no cabeçalho

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/components/account-menu.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/components/account-menu.tsx
@@ -9,8 +9,21 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { useQuery } from '@tanstack/react-query'
+import { getProfile } from '@/api/get-profile'
 
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+  
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,16 +31,16 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Taiane Karine</span>
+          <span>{profile?.name}</span>
           <span className="text-xs font-normal text-muted-foreground">
-            taianekas@tksresolutions.com.br
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
