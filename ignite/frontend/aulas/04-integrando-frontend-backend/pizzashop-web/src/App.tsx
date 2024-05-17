bb451c6bae469bbf38116d79051bed4bfe63fefd
taianekas <taianekas.dev@gmail.com>
2024-05-16 22:05:08 -0300

    ✨ Notificações toast (sonner)

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/App.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/App.tsx
@@ -2,12 +2,14 @@ import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { Toaster } from 'sonner'
+
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
-
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/auth/sign-in.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from 'zod'
 
 const signInForm = z.object({
@@ -16,8 +17,18 @@ export function SignIn() {
   } } = useForm<SignInForm>()
 
   const handleSignIn = async (data: SignInForm) => {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+  
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data)
+        }
+      })
+    } catch {
+      toast.error('Credenciais inválidas.')
+    }
   } 
 
   return (
