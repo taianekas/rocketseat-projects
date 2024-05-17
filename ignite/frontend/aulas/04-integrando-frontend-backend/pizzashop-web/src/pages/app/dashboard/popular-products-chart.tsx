50113a41909d003da3068804b95d2df12490d281
taianekas <taianekas.dev@gmail.com>
2024-05-17 15:09:45 -0300

    ✨ Loading dos pedidos

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,4 +1,4 @@
-import { BarChart } from 'lucide-react'
+import { BarChart, Loader2 } from 'lucide-react'
 import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
@@ -32,7 +32,7 @@ export function PopularProductsChart() {
         </div>
       </CardHeader>
       <CardContent>
-        { popularProducts && (
+        { popularProducts ? (
           <ResponsiveContainer width="100%" height={240}>
             <PieChart style={{ fontSize: 12 }}>
               <Pie
@@ -89,6 +89,10 @@ export function PopularProductsChart() {
               </Pie>
             </PieChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
