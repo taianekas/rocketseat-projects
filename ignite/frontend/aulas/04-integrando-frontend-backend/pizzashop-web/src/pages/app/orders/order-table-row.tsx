96c156da7194a401ef1d3bcbe6f72322bc87d3ce
taianekas <taianekas.dev@gmail.com>
2024-05-17 14:10:22 -0300

    ✨ Cancelar pedido

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/orders/order-table-row.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -8,6 +8,9 @@ import { formatDistanceToNow } from 'date-fns'
 import { OrderDetails } from './order-details'
 import { OrderStatus } from '@/components/order-status'
 import { useState } from 'react'
+import { cancelOrder } from '@/api/cancel-order'
+import { GetOrdersResponse } from '@/api/get-orders'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 interface OrderTableRowProps {
   order: {
@@ -21,6 +24,34 @@ interface OrderTableRowProps {
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
   const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+  
+  const queryClient = useQueryClient()
+
+  const { mutateAsync: cancelOrderFn } = useMutation({
+    mutationFn: cancelOrder,
+    async onSuccess(_, { orderId }) {
+      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey: ['orders'],
+      })
+
+      ordersListCache.forEach(([cacheKey, cacheData]) => {
+        if (!cacheData) {
+          return
+        }
+
+        queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+          ...cacheData,
+          orders: cacheData.orders.map((order) => {
+            if (order.orderId === orderId) {
+              return { ...order, status: 'canceled' }
+            }
+
+            return order
+          }),
+        })
+      })
+    },
+  })
   
   return (
     <TableRow>
@@ -62,7 +93,12 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
       </Button>
     </TableCell>
     <TableCell>
-      <Button variant="ghost" size="xs">
+      <Button
+        disabled={!['pending', 'processing'].includes(order.status)}
+        onClick={() => cancelOrderFn({ orderId: order.orderId })}
+        variant="ghost"
+        size="xs"
+      >
         <X className="mr-2 h-3 w-3" />
         Cancelar
       </Button>
