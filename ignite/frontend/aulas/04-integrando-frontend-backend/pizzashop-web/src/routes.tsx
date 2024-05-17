cdcbaa8e9d939a556c1685dc3f4c3db78532e787
taianekas <taianekas.dev@gmail.com>
2024-05-17 00:54:29 -0300

    ✨ Page: 404 (Not Found)

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/routes.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/routes.tsx
@@ -6,11 +6,13 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
 import { Orders } from './pages/app/orders/order'
+import { NotFound } from './pages/404'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
