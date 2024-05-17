31564fd76f31bf5c69d6e1f0803c4572a722a984
taianekas <taianekas.dev@gmail.com>
2024-05-17 13:54:11 -0300

    ✨ Detalhes do pedido

--- ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/orders/order-table-row.tsx
+++ ignite/frontend/aulas/04-integrando-frontend-backend/pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -7,6 +7,7 @@ import { ptBR } from 'date-fns/locale'
 import { formatDistanceToNow } from 'date-fns'
 import { OrderDetails } from './order-details'
 import { OrderStatus } from '@/components/order-status'
+import { useState } from 'react'
 
 interface OrderTableRowProps {
   order: {
@@ -19,11 +20,12 @@ interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
-
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+  
   return (
     <TableRow>
       <TableCell>
-        <Dialog>
+        <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
           <DialogTrigger asChild>
             <Button variant="outline" size="xs">
               <Search className="h-3 w-3" />
@@ -31,7 +33,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
             </Button>
           </DialogTrigger>
 
-          <OrderDetails />
+          <OrderDetails open={isDetailsOpen} orderId={order.orderId} />
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
@@ -48,7 +50,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
     </TableCell>
     <TableCell className="font-medium">{order.customerName}</TableCell>
     <TableCell className="font-medium">
-      {order.total.toLocaleString('pt-BR', {
+      {(order.total / 100).toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL',
       })}
