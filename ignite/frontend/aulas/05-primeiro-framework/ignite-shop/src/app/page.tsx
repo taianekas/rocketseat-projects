5bde44a5cce4f24a278207fe0990d7dfca7f2fb6
taianekas <taianekas.dev@gmail.com>
2024-05-18 16:18:47 -0300

    Estilos globais

--- ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/app/page.tsx
+++ ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/app/page.tsx
@@ -1,5 +1,25 @@
+import { styled } from "../styles"
+
+ const Button = styled('button', {
+   backgroundColor: '$green500',
+   borderRadius: 4,
+   border: 0,
+   padding: '4px 8px',
+
+   span: {
+     fontWeight: 'bold'
+   },
+
+   '&:hover': {
+     filter: 'brightness(0.8)'
+   }
+ })
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <Button>
+      <span>Testedassd</span>
+      Enviar
+    </Button>
   );
 }
