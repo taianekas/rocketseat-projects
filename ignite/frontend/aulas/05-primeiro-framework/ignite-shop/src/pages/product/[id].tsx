cc999fe0050b26301c6848b1fe1998457b34c4be
taianekas <taianekas.dev@gmail.com>
2024-05-20 13:26:22 -0300

    Page: Product

---
+++ ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/pages/product/[id].tsx
@@ -0,0 +1,25 @@
+import { ProductContainer, ImageContainer, ProductDetails } from "@/styles/pages/product"
+import { useRouter } from "next/router"
+
+
+export default function Product() {
+  const { query } = useRouter()
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores aliquid rerum exercitationem facere a molestiae ut sed velit non mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed laboriosam tempora?</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  )
+}
