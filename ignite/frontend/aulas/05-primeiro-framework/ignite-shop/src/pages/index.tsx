abf26b58afbc99e5f3c26427e3e267302b17cc46
taianekas <taianekas.dev@gmail.com>
2024-05-20 13:08:14 -0300

    Introduzindo SSG

--- ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/pages/index.tsx
+++ ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { HomeContainer, Product } from "@/styles/pages/home"
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
 import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 
 import Stripe from "stripe"
 
@@ -42,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
@@ -62,6 +62,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2,
   }
 }
