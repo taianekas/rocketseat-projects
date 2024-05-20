ddf170a9a08906104b2730591760265479b2c051
taianekas <taianekas.dev@gmail.com>
2024-05-20 13:26:22 -0300

    Page: Product

--- ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/pages/index.tsx
+++ ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/pages/index.tsx
@@ -6,6 +6,7 @@ import { stripe } from "../lib/stripe"
 import { GetStaticProps } from "next"
 
 import Stripe from "stripe"
+import Link from "next/link"
 
  interface HomeProps {
    products: {
@@ -28,14 +29,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
@@ -55,7 +58,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount === null ? 0 : price.unit_amount / 100
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount === null ? 0 : price.unit_amount / 100)
     }
   })
 
