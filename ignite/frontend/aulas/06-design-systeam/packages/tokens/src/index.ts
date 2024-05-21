5cfdcf92f63bad2e8bcf12aa84090b842a04b65f
taianekas <taianekas.dev@gmail.com>
2024-05-21 19:25:42 -0300

    Configurando tokens

--- ignite/frontend/aulas/06-design-systeam/packages/tokens/src/index.ts
+++ ignite/frontend/aulas/06-design-systeam/packages/tokens/src/index.ts
@@ -1,1 +1,9 @@
 export * from './colors'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
+
+export * from './radii'
+export * from './space'
