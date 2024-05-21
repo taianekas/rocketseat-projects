09c2ab94bd8d775919242b8950606bc08b153425
taianekas <taianekas.dev@gmail.com>
2024-05-21 03:19:08 -0300

    Finalizando page Product

--- ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/pages/product/[id].tsx
+++ ignite/frontend/aulas/05-primeiro-framework/ignite-shop/src/pages/product/[id].tsx
@@ -1,25 +1,108 @@
+import Image from "next/image"
+import { useState } from "react"
+import Stripe from "stripe"
+import { stripe } from "@/lib/stripe"
+import { GetStaticPaths, GetStaticProps } from "next"
+import axios from "axios"
 import { ProductContainer, ImageContainer, ProductDetails } from "@/styles/pages/product"
-import { useRouter } from "next/router"
 
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+    defaultPriceId: string
+  }
+}
 
-export default function Product() {
-  const { query } = useRouter()
+const useBuyProduct = (product: ProductProps['product']) => {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+
+  const handleBuyProductButton = async () => {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (err) {
+      setIsCreatingCheckoutSession(false);
+
+      alert('Falha ao redirecionar ao checkout!')
+    }
+  };
+
+  return { isCreatingCheckoutSession, handleBuyProductButton };
+};
+
+export default function Product({ product }: ProductProps) {
+  const { isCreatingCheckoutSession, handleBuyProductButton } = useBuyProduct(product);
+
 
   return (
     <ProductContainer>
       <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores aliquid rerum exercitationem facere a molestiae ut sed velit non mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed laboriosam tempora?</p>
+          <p>{product.description}</p>
 
-        <button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProductButton}>
           Comprar agora
         </button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: 'prod_MLH5Wy0Y97hDAC' } },
+    ],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+  
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount === null ? 0 : price.unit_amount / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      }
+    },
+    revalidate: 60 * 60 * 1 // 1 hours
+  }
 }
