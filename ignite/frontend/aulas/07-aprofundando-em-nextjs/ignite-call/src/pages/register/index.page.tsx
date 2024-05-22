1a4cb0faa74ec319edd950e7874b97a5027a3cb4
taianekas <taianekas.dev@gmail.com>
2024-05-21 22:37:32 -0300

    Valor inicial do nome de usuário

--- ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/pages/register/index.page.tsx
+++ ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/pages/register/index.page.tsx
@@ -6,6 +6,9 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 const registerFormSchema = z.object({
   username: z
     .string()
@@ -25,10 +28,19 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
   function handleRegister(data: RegisterFormData) {
     console.log(data)
