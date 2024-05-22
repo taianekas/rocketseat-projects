55f44aee2183c2c6ad92f40c56a163ce8fea1ca6
taianekas <taianekas.dev@gmail.com>
2024-05-22 14:15:06 -0300

    Alterando banco para MySQL

--- ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/components/Calendar/index.tsx
+++ ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/components/Calendar/index.tsx
@@ -75,6 +75,10 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   )
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -112,7 +116,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
