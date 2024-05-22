5e50b0df2783376745ffad4606cb06a701f2f3c5
taianekas <taianekas.dev@gmail.com>
2024-05-22 13:34:05 -0300

    Calendário e disponibilidade - Preparando calendário

--- ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,13 +1,16 @@
 import { Calendar } from '@/components/Calendar'
 import { Container, TimePicker, TimePickerHeader, TimePickerList, TimePickerItem } from './styles'
+import { useState } from 'react'
 
 
 export function CalendarStep() {
-  const isDateSelected = false
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+  const isDateSelected = !!selectedDate
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar />
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 
       {isDateSelected && (
         <TimePicker>
