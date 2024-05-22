e9f61607c4866511a64529698bf6cec681b755b1
taianekas <taianekas.dev@gmail.com>
2024-05-22 12:27:07 -0300

    Página de disponibilidade

--- ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/pages/register/connect-calendar/index.page.tsx
+++ ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/pages/register/connect-calendar/index.page.tsx
@@ -6,7 +6,7 @@ import { ConnectBox, ConnectItem, AuthError } from './styles'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
-export default function Register() {
+export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
 
