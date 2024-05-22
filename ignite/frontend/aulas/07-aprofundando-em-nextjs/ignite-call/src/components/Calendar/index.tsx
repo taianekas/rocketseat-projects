c098e62b453dbdafe8249dbc7da2cc676ed271b1
taianekas <taianekas.dev@gmail.com>
2024-05-22 14:18:32 -0300

    Calendário e disponibilidade - Finalizando query de dias lotados

--- ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/components/Calendar/index.tsx
+++ ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/components/Calendar/index.tsx
@@ -17,6 +17,7 @@ import { api } from '@/lib/axios'
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarWeek {
@@ -66,7 +67,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: currentDate.get('month') + 1,
         },
       })
 
@@ -78,6 +79,8 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
     if (!blockedDates) {
       return []
     }
+
+    console.log('calendarWeeks ~ blockedDates', blockedDates)
 
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
@@ -116,7 +119,8 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
