b1cc48effc67c4db6f5dbde531351d996819d5ed
taianekas <taianekas.dev@gmail.com>
2024-05-22 13:27:25 -0300

    Calculando dias do mês

--- ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/components/Calendar/index.tsx
+++ ignite/frontend/aulas/07-aprofundando-em-nextjs/ignite-call/src/components/Calendar/index.tsx
@@ -10,7 +10,7 @@ import {
 } from './styles'
 
 import dayjs from 'dayjs'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 export function Calendar() {
   const [currentDate, setCurrentDate] = useState(() => {
@@ -34,6 +34,29 @@ export function Calendar() {
   const currentMonth = currentDate.format('MMMM')
   const currentYear = currentDate.format('YYYY')
 
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day')
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
+  
   return (
     <CalendarContainer>
       <CalendarHeader>
